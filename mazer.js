e1f13242dfa1b33ba5e8ab44ad2c93bc16431ee0
Logan Bell <loganbell@gmail.com>
2025-07-13 14:10:48 -0700

    Adding random end point

--- mazer.js
+++ mazer.js
@@ -6,31 +6,32 @@ for (let x = 0; x < 10; x++) {
       _visited: false,
       coords: [x, y],
       walls: { top: true, right: true, bottom: true, left: true },
-    }
+    };
   }
 }
 
 let randomStartx;
 let randomStarty;
+let randomEndx;
+let randomEndy;
 
 export const buildMaze = () => {
-
-  const _buildMaze = cell => {
+  const _buildMaze = (cell) => {
     cell._visited = true;
 
     const neighbors = [
-      [maze[cell.coords[0] - 1]?.[cell.coords[1]], 'top'],
-      [maze[cell.coords[0] + 1]?.[cell.coords[1]], 'bottom'],
-      [maze[cell.coords[0]]?.[cell.coords[1] - 1], 'left'],
-      [maze[cell.coords[0]]?.[cell.coords[1] + 1], 'right'],
+      [maze[cell.coords[0] - 1]?.[cell.coords[1]], "top"],
+      [maze[cell.coords[0] + 1]?.[cell.coords[1]], "bottom"],
+      [maze[cell.coords[0]]?.[cell.coords[1] - 1], "left"],
+      [maze[cell.coords[0]]?.[cell.coords[1] + 1], "right"],
     ];
 
-    const unvisitedNeighbors = []
+    const unvisitedNeighbors = [];
     for (const n of neighbors) {
       if (n && n[0] && !n[0]._visited) {
         unvisitedNeighbors.push(n);
       }
-    };
+    }
 
     while (unvisitedNeighbors.length) {
       const idx = parseInt(Math.random() * unvisitedNeighbors.length);
@@ -39,25 +40,28 @@ export const buildMaze = () => {
       const wall = neighbor[1];
 
       if (!neighborCell._visited) {
-        const neighborWall = wall == 'top' && 'bottom' ||
-              wall == 'bottom' && 'top' ||
-              wall == 'left' && 'right' ||
-              wall == 'right' && 'left';
+        const neighborWall = wall == "top" && "bottom" ||
+          wall == "bottom" && "top" ||
+          wall == "left" && "right" ||
+          wall == "right" && "left";
         cell.walls[wall] = false;
         neighborCell.walls[neighborWall] = false;
         _buildMaze(neighborCell);
       }
     }
-  }
+  };
 
   randomStartx = parseInt(Math.random() * maze.length);
   randomStarty = parseInt(Math.random() * maze[randomStartx].length);
+
+  randomEndx = parseInt(Math.random() * maze.length);
+  randomEndy = parseInt(Math.random() * maze[randomEndx].length);
+
   _buildMaze(maze[randomStartx][randomStarty]);
-  return maze
+  return maze;
+};
 
-}
-
-export const mazeToGraph = maze => {
+export const mazeToGraph = (maze) => {
   let graph = {};
   for (let x = 0; x < maze.length; x++) {
     for (let y = 0; y < maze[x].length; y++) {
@@ -74,46 +78,47 @@ export const mazeToGraph = maze => {
         }
       }
 
-      graph[`[${x},${y}]`] = []
+      graph[`[${x},${y}]`] = [];
       for (const n of Object.values(possibleNeighbors)) {
-        graph[`[${x},${y}]`].push(JSON.stringify(n.coords))
+        graph[`[${x},${y}]`].push(JSON.stringify(n.coords));
       }
     }
   }
   return graph;
-}
-
+};
 
 export const renderMaze = (mazeEl, maze) => {
-
   for (let x = 0; x < 10; x++) {
     for (let y = 0; y < 10; y++) {
-
       const cell = maze[x][y];
-      const coords = cell.coords
+      const coords = cell.coords;
 
-      const cellEl = document.createElement('div');
-      const wallLeft = document.createElement('div');
-      wallLeft.classList.add('wall-left');
-      const wallTop = document.createElement('div');
-      wallTop.classList.add('wall-top');
+      const cellEl = document.createElement("div");
+      const wallLeft = document.createElement("div");
+      wallLeft.classList.add("wall-left");
+      const wallTop = document.createElement("div");
+      wallTop.classList.add("wall-top");
       wallLeft.style = `top: ${coords[0] * 50}px; left: ${coords[1] * 50}px;`;
       wallTop.style = `top: ${coords[0] * 50}px; left: ${coords[1] * 50}px;`;
 
-      cellEl.classList.add('cell');
+      cellEl.classList.add("cell");
       cellEl.style.left = `${coords[1] * 50 + (x > 0 && y > 0 ? 1 : 0)}px`;
       cellEl.style.top = `${coords[0] * 50 + (x > 0 && y > 0 ? 1 : 0)}px`;
 
       if (randomStartx == x && randomStarty == y) {
-        cellEl.innerHTML = 'Start'
+        cellEl.innerHTML = "Start";
+      }
+
+      if (randomEndx == x && randomEndy == y) {
+        cellEl.innerHTML = "End";
       }
 
       for (const [key, val] of Object.entries(cell.walls)) {
-        if (key == 'top' && !val) {
-          wallTop.style.background = 'none';
+        if (key == "top" && !val) {
+          wallTop.style.background = "none";
         }
-        if (key == 'left' && !val) {
-          wallLeft.style.background = 'none';
+        if (key == "left" && !val) {
+          wallLeft.style.background = "none";
         }
       }
       mazeEl.appendChild(cellEl);
@@ -125,5 +130,4 @@ export const renderMaze = (mazeEl, maze) => {
       }
     }
   }
-}
-
+};
